19075313dc7d2ed5bd71ce92623db06d59ab9f40
Tyler Howard <tyler.howard.dev@gmail.com>
2024-12-01 15:28:26 -0500

    Update template to my liking

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,21 +1,23 @@
+fun part1(input: List<String>): Int {
+    return input.size
+}
+
+fun part2(input: List<String>): Int {
+    return input.size
+}
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
-
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
-
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+//    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    val testInput = readInput("inputs/Day01_test")
+    val expectedOutput = 1
+    val actualOutput = part1(testInput)
+    check(actualOutput == expectedOutput) { "Expected $expectedOutput but received $actualOutput" }
 
     // Read the input from the `src/Day01.txt` file.
-    val input = readInput("Day01")
+    val input = readInput("inputs/Day01")
     part1(input).println()
     part2(input).println()
 }
