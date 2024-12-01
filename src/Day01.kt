a341ffb19dd4a20cc61a96437ebe3b5a79ef88ee
Tyler Howard <tyler.howard.dev@gmail.com>
2024-12-01 15:49:20 -0500

    Working solution for day1 part1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,5 +1,22 @@
+import kotlin.math.abs
+
 fun part1(input: List<String>): Int {
-    return input.size
+    val left = mutableListOf<Int>()
+    val right = mutableListOf<Int>()
+    val distances = mutableListOf<Int>()
+
+    input.forEach {
+        val pairRegex = "(\\d+)\\s+(\\d+)".toRegex()
+        val matches = pairRegex.find(it) ?: throw Exception("Invalid input")
+        left.add(matches.groupValues[1].toInt())
+        right.add(matches.groupValues[2].toInt())
+    }
+    left.sort()
+    right.sort()
+
+    left.forEachIndexed { index, _ -> distances.add(abs(right[index] - left[index])) }
+
+    return distances.sum()
 }
 
 fun part2(input: List<String>): Int {
@@ -8,7 +25,7 @@ fun part2(input: List<String>): Int {
 
 fun main() {
     // Test if implementation meets criteria from the description, like:
-//    check(part1(listOf("test_input")) == 1)
+    //    check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("inputs/Day01_test")
@@ -19,5 +36,5 @@ fun main() {
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("inputs/Day01")
     part1(input).println()
-    part2(input).println()
+//    part2(input).println()
 }
